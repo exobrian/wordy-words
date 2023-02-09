6678c25a94926162b8ed6ada5158883d7cd7b833
Brian Tran <exobrian@yahoo.com>
2023-02-08 21:34:04 -0800

    latest changes

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 // This script reads in list of words stored in json file. Then creates a wordDictionary where the key corresponds to the number of letters
 // in a word, while the value is an array containing all the words with length equal to the key.
 
-var wordsPath = './resources/wordsList.json';
-const WORD_LENGTH_SELECTED = 6;
-const words = require(wordsPath);
-
+const wordsPath = './resources/wordsList.json';
+const words = require(wordsPath)
+//let wordLengthSelected = document.getElementById("numberOfLetters").value;
+let wordLengthSelected = 8
 // Parse through json list of words. Create wordDictionary using lengths of words as keys and keep track of the longest word.
 let maxWordLength = 0;
 let longestWord = "";
 let wordDictionary = {};
-for (let key in words){
-    if (key.length > maxWordLength){
-        maxWordLength = key.length;
-        longestWord = key;
-    }
-    if (key.length in wordDictionary){
-        wordDictionary[key.length].push(key);
-    } else {
-        wordDictionary[key.length] = [key];
+
+function createDictionary() {
+    for (let key in words){
+        if (key.length > maxWordLength){
+            maxWordLength = key.length;
+            longestWord = key;
+        }
+        if (key.length in wordDictionary){
+            wordDictionary[key.length].push(key);
+        } else {
+            wordDictionary[key.length] = [key];
+        }
     }
 }
 
-let randInt = Math.floor(Math.random() * (wordDictionary[WORD_LENGTH_SELECTED].length - 1));
-let randWord = wordDictionary[WORD_LENGTH_SELECTED][randInt];
+function isValidLength() {
+    console.log(wordLengthSelected in wordDictionary ? "Word Length is valid" : "Word Length is invalide");
+    return;
+}
+
+createDictionary();
+let randInt = Math.floor(Math.random() * (wordDictionary[wordLengthSelected].length - 1));
+let randWord = wordDictionary[wordLengthSelected][randInt];
 
 // Testing
 console.log("Random Word: " + randWord);
