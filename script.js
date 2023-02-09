8fbee3fe9ec32c8b3eef0fff4d225af9891d6754
Brian Tran <exobrian@yahoo.com>
2023-02-08 22:46:05 -0800

    implement api to fetch dictionary json from github

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-// This script reads in list of words stored in json file. Then creates a wordDictionary where the key corresponds to the number of letters
-// in a word, while the value is an array containing all the words with length equal to the key.
-
-const wordsPath = './resources/wordsList.json';
-const words = require(wordsPath)
-//let wordLengthSelected = document.getElementById("numberOfLetters").value;
-let wordLengthSelected = 8
-// Parse through json list of words. Create wordDictionary using lengths of words as keys and keep track of the longest word.
+const wordsPath = "./resources/wordsList.json";
+const wordsURL = "https://raw.githubusercontent.com/exobrian/Wordy-Words/main/resources/wordsList.json";
 let maxWordLength = 0;
 let longestWord = "";
 let wordDictionary = {};
 
-function createDictionary() {
+//Initialize with 5 letters to avoid ref errors
+let wordLengthSelected = 5;
+
+// Need to run this first in order to force other functions to wait until the json file has been loaded.
+fetch(wordsURL)
+.then(response => response.json())
+.then(json => {
+    createDictionary(json);
+    let randInt = Math.floor(Math.random() * (wordDictionary[wordLengthSelected].length - 1));
+    let randWord = wordDictionary[wordLengthSelected][randInt];
+    console.log("Random Word: " + randWord);
+    console.log("Test: " + wordDictionary[maxWordLength][0]);
+    console.log("Longest word is " + longestWord + ": " + maxWordLength + " letters");
+});
+
+function createDictionary(words) {
+    // Parse through json list of words. Create wordDictionary using lengths of words as keys and keep track of the longest word.
     for (let key in words){
         if (key.length > maxWordLength){
             maxWordLength = key.length;
@@ -25,15 +35,9 @@ function createDictionary() {
 }
 
 function isValidLength() {
-    console.log(wordLengthSelected in wordDictionary ? "Word Length is valid" : "Word Length is invalide");
+    // function returns true if word length selected by the user exists in our dictionary
+    wordLengthSelected = document.getElementById("numberOfLetters").value;
+    console.log("Word Length is " + wordLengthSelected + " letters long.");
+    console.log(wordLengthSelected in wordDictionary ? "Word Length is valid." : "Word Length is invalid.");
     return;
 }
-
-createDictionary();
-let randInt = Math.floor(Math.random() * (wordDictionary[wordLengthSelected].length - 1));
-let randWord = wordDictionary[wordLengthSelected][randInt];
-
-// Testing
-console.log("Random Word: " + randWord);
-console.log("Test: " + wordDictionary[maxWordLength][0]);
-console.log("Longest word is " + longestWord + ": " + maxWordLength + " letters");
